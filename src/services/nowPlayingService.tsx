118386b93b3d9edf37e8a4cc88b10d18a88a1172
Parikshith Mohite <parimohite1@gmail.com>
2024-09-01 14:01:33 -0400

    Implements logic to get current audio features

---
+++ src/services/nowPlayingService.tsx
@@ -0,0 +1,23 @@
+import AudioFeaturesResponse from "../types/spotify-web-api";
+
+async function getAudioFeatures(songURI: string | undefined): Promise<AudioFeaturesResponse | {}> {
+    if (!songURI) {
+        return {};
+    }
+
+    var accessToken = Spicetify.Platform.Session.accessToken;
+
+    var songID = songURI.split(":")[2];
+    let response = await fetch(
+        "https://api.spotify.com/v1/audio-features/" + songID,
+        {
+            headers: {
+                Authorization: "Bearer " + accessToken,
+            },
+        }
+    );
+
+    return response.status == 200 ? await response.json() : {};
+}
+
+export default getAudioFeatures;
---
+++ src/types/spotify-web-api.d.ts
@@ -0,0 +1,22 @@
+type AudioFeaturesResponse = {
+  acousticness: string,
+  analysis_url: string,
+  danceability: string,
+  duration_ms: string,
+  energy: string,
+  id: string,
+  instrumentalness: string,
+  key: string,
+  liveness: string,
+  loudness: string,
+  mode: string,
+  speechiness: string,
+  tempo: string,
+  time_signature: string,
+  track_href: string,
+  type: string,
+  uri: string,
+  valence: string
+};
+
+export default AudioFeaturesResponse;
