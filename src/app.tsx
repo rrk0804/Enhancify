8792a4f26b2999c815245cbda1526eed56c18e95
Parikshith Mohite <parimohite1@gmail.com>
2024-09-27 11:34:20 -0400

    Remove extra call to DynamicRecommendations in app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import NowPlaying from "./components/NowPlaying";
-import DynamicRecommendations from "./components/DynamicRecommendations";
 
 class App extends React.Component<{}, {}> {
   render() {
     return (
       <>
         <NowPlaying />
-        <DynamicRecommendations />
       </>
     );
   }
